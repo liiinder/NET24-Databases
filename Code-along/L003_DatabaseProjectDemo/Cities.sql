ec19dc41d8214b885ea8f2d25ae000dc6e89b6ee
Fredrik Johansson <fredrik@everyloop.com>
2024-11-27 11:21:58 +0100

    Add L003

---
+++ Code-along/L003_DatabaseProjectDemo/Cities.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Cities]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(50) NULL, 
+    [CountryId] INT NULL, 
+    CONSTRAINT [FK_Cities_Countries] FOREIGN KEY ([CountryId]) REFERENCES [Countries]([Id])
+)
---
+++ Code-along/L003_DatabaseProjectDemo/Countries.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Countries]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NVARCHAR(50) NULL,
+
+)
