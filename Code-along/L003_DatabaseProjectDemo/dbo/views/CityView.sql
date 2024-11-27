cb0478a1ca10b08bffaf81dce872565243f457ba
Fredrik Johansson <fredrik@everyloop.com>
2024-11-27 11:50:48 +0100

    Update L003, Add CityView

---
+++ Code-along/L003_DatabaseProjectDemo/dbo/views/CityView.sql
@@ -0,0 +1,10 @@
+﻿CREATE VIEW [dbo].[CityView] AS
+select 
+	[ci].[Id], 
+	[ci].[Name] as 'City', 
+	[ci].[CountryId], 
+	[co].[Name] as 'Country' 
+from 
+	[dbo].[Cities] ci
+	left join [dbo].[Countries] co on ci.CountryId = co.Id
+
