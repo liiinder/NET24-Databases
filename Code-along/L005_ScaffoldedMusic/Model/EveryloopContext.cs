0511a74115df9f6003964d961ee6b804be2732ed
Fredrik Johansson <fredrik@everyloop.com>
2024-12-02 19:51:23 +0100

    Add L005

---
+++ Code-along/L005_ScaffoldedMusic/Model/EveryloopContext.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace L005_ScaffoldedMusic.Model;
+
+public partial class EveryloopContext : DbContext
+{
+    public EveryloopContext()
+    {
+    }
+
+    public EveryloopContext(DbContextOptions<EveryloopContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Album> Albums { get; set; }
+
+    public virtual DbSet<Artist> Artists { get; set; }
+
+    public virtual DbSet<Genre> Genres { get; set; }
+
+    public virtual DbSet<MediaType> MediaTypes { get; set; }
+
+    public virtual DbSet<Playlist> Playlists { get; set; }
+
+    public virtual DbSet<Track> Tracks { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Initial Catalog=everyloop;Integrated Security=True;Trust Server Certificate=True;Server SPN=localhost");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.UseCollation("Finnish_Swedish_CI_AS");
+
+        modelBuilder.Entity<Album>(entity =>
+        {
+            entity.ToTable("albums", "music");
+
+            entity.Property(e => e.AlbumId).ValueGeneratedNever();
+            entity.Property(e => e.Title).HasMaxLength(160);
+
+            entity.HasOne(d => d.Artist).WithMany(p => p.Albums)
+                .HasForeignKey(d => d.ArtistId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_albums_artists");
+        });
+
+        modelBuilder.Entity<Artist>(entity =>
+        {
+            entity.ToTable("artists", "music");
+
+            entity.Property(e => e.ArtistId).ValueGeneratedNever();
+            entity.Property(e => e.Name).HasMaxLength(120);
+        });
+
+        modelBuilder.Entity<Genre>(entity =>
+        {
+            entity.ToTable("genres", "music");
+
+            entity.Property(e => e.GenreId).ValueGeneratedNever();
+            entity.Property(e => e.Name).HasMaxLength(120);
+        });
+
+        modelBuilder.Entity<MediaType>(entity =>
+        {
+            entity.ToTable("media_types", "music");
+
+            entity.Property(e => e.MediaTypeId).ValueGeneratedNever();
+            entity.Property(e => e.Name).HasMaxLength(120);
+        });
+
+        modelBuilder.Entity<Playlist>(entity =>
+        {
+            entity.ToTable("playlists", "music");
+
+            entity.Property(e => e.PlaylistId).ValueGeneratedNever();
+            entity.Property(e => e.Name).HasMaxLength(120);
+
+            entity.HasMany(d => d.Tracks).WithMany(p => p.Playlists)
+                .UsingEntity<Dictionary<string, object>>(
+                    "PlaylistTrack",
+                    r => r.HasOne<Track>().WithMany()
+                        .HasForeignKey("TrackId")
+                        .OnDelete(DeleteBehavior.ClientSetNull)
+                        .HasConstraintName("FK_playlist_track_tracks"),
+                    l => l.HasOne<Playlist>().WithMany()
+                        .HasForeignKey("PlaylistId")
+                        .OnDelete(DeleteBehavior.ClientSetNull)
+                        .HasConstraintName("FK_playlist_track_playlists"),
+                    j =>
+                    {
+                        j.HasKey("PlaylistId", "TrackId");
+                        j.ToTable("playlist_track", "music");
+                    });
+        });
+
+        modelBuilder.Entity<Track>(entity =>
+        {
+            entity.ToTable("tracks", "music");
+
+            entity.Property(e => e.TrackId).ValueGeneratedNever();
+            entity.Property(e => e.Composer).HasMaxLength(220);
+            entity.Property(e => e.Name).HasMaxLength(200);
+
+            entity.HasOne(d => d.Album).WithMany(p => p.Tracks)
+                .HasForeignKey(d => d.AlbumId)
+                .HasConstraintName("FK_tracks_albums");
+
+            entity.HasOne(d => d.Genre).WithMany(p => p.Tracks)
+                .HasForeignKey(d => d.GenreId)
+                .HasConstraintName("FK_tracks_genres");
+
+            entity.HasOne(d => d.MediaType).WithMany(p => p.Tracks)
+                .HasForeignKey(d => d.MediaTypeId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_tracks_media_types");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
