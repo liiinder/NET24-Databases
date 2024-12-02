5155ba62b4d0a2ddfb269449884580b77c397b0a
Fredrik Johansson <fredrik@everyloop.com>
2024-12-02 14:32:09 +0100

    Add L004

---
+++ Code-along/L004_Intro_EFcore/Model/BloggingContext.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Principal;
+
+namespace L004_Intro_EFcore.Model;
+
+public class BloggingContext : DbContext
+{
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<Post> Posts { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = new SqlConnectionStringBuilder()
+        {
+            ServerSPN = "localhost",
+            InitialCatalog = "BlogDB",
+            TrustServerCertificate = true,
+            IntegratedSecurity = true
+        }.ToString();
+
+        optionsBuilder.UseSqlServer(connectionString);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
+
+public class Blog
+{
+    public int BlogId { get; set; }
+    public string? Url { get; set; }
+    public int? Rating { get; set; }
+    public List<Post> Posts { get; set; }
+}
+
+public class Post
+{
+    public int PostId { get; set; }
+    public string Topic { get; set; }
+    public string Text { get; set; }
+}
---
+++ Code-along/L004_Intro_EFcore/Program.cs
@@ -0,0 +1,60 @@
+﻿
+using L004_Intro_EFcore.Model;
+using Microsoft.EntityFrameworkCore;
+
+//EnsureDatabaseIsDeleted();
+//EnsureDatabaseIsCreated();
+
+CreateBlog("yetanotherblog.com", 2);
+
+var myBlogs = GetBlogs(printSQL: true);
+
+foreach (var blog in myBlogs)
+{
+    Console.WriteLine($"{blog.Url}, rating: {blog.Rating}");
+}
+
+static void CreateBlog(string url, int rating = 5)
+{
+    var blog = new Blog() { Url = url, Rating = rating };
+
+    using var db = new BloggingContext();
+
+    db.Blogs.Add(blog);
+
+    db.SaveChanges();
+}
+
+static List<Blog> GetBlogs(string searchString = "", bool printSQL = false)
+{
+    using var db = new BloggingContext();
+
+    var query = db.Blogs.Where(b => b.Url.Contains(searchString)).OrderByDescending(b => b.Rating).ThenBy(b => b.Url);
+    
+    if (printSQL)
+    {
+        Console.WriteLine(query.ToQueryString());
+        Console.WriteLine();
+    }
+
+    return query.ToList();
+}
+
+static List<Post> GetAllPosts()
+{
+    using var db = new BloggingContext();
+    return db.Posts.ToList();
+}
+static void EnsureDatabaseIsCreated()
+{ 
+    using var db = new BloggingContext();
+    
+    db.Database.EnsureCreated();
+}
+
+static void EnsureDatabaseIsDeleted()
+{
+    using var db = new BloggingContext();
+
+    db.Database.EnsureDeleted();
+}
