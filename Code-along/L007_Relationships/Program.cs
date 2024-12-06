79a6bc7beca53aeecd0356a762219ba9c7152b57
Fredrik Johansson <fredrik@everyloop.com>
2024-12-06 12:47:06 +0100

    Update L007

--- Code-along/L007_Relationships/Program.cs
+++ Code-along/L007_Relationships/Program.cs
@@ -1,8 +1,15 @@
 ﻿
 using L007_Relationships.Model;
+using Microsoft.EntityFrameworkCore;
 
 using var db = new RelationshipDemoContext();
 
 db.Database.EnsureDeleted();
 db.Database.EnsureCreated();
 
+var query = db.Courses.Include(c => c.Students);
+
+var course = query.ToList();
+
+Console.WriteLine(query.ToQueryString());
+Console.WriteLine();
