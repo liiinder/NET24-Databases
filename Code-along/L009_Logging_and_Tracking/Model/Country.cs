f9b705eb1d85ed1c6886580771b18df33699a8ac
Fredrik Johansson <fredrik@everyloop.com>
2024-12-09 10:14:02 +0100

    Add L009, with examples of bitwise OR.

---
+++ Code-along/L009_Logging_and_Tracking/Model/Country.cs
@@ -0,0 +1,8 @@
+﻿namespace L009_Logging_and_Tracking.Model;
+
+public class Country
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public List<City> Cities { get; set; }
+}
---
+++ Code-along/L009_Logging_and_Tracking/Model/DemoContext.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace L009_Logging_and_Tracking.Model;
+
+internal class DemoContext : DbContext
+{
+    public DbSet<Country> Countries { get; set; }
+    public DbSet<City> Cities { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var connectionString = new SqlConnectionStringBuilder()
+        {
+            ServerSPN = "localhost",
+            InitialCatalog = "CityDB",
+            IntegratedSecurity = true,
+            TrustServerCertificate = true
+        }.ToString();
+
+        optionsBuilder
+            .UseSqlServer(connectionString)
+            .LogTo(
+                message => MyLogger(message),
+                new[] { DbLoggerCategory.Database.Command.Name },
+                LogLevel.Information,
+                DbContextLoggerOptions.Level | DbContextLoggerOptions.LocalTime
+            );
+    }
+
+    private void MyLogger(string message)
+    {
+        var lines = message.Split('\n');
+
+        Console.WriteLine();
+
+        for (int i = 0; i < lines.Length; i++)
+        {
+            if (i == 0) Console.ForegroundColor = ConsoleColor.Blue;
+            if (i == 1) Console.ForegroundColor = ConsoleColor.DarkGray;
+            if (i == 2) Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine(lines[i]);
+        }
+
+        Console.ResetColor();
+        Console.WriteLine();
+    }
+}
