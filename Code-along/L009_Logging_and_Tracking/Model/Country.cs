ddec799b989387e87fcfcb95106a3b7650c4072f
Fredrik Johansson <fredrik@everyloop.com>
2024-12-09 14:18:28 +0100

    Update L009

--- Code-along/L009_Logging_and_Tracking/Model/Country.cs
+++ Code-along/L009_Logging_and_Tracking/Model/Country.cs
@@ -4,5 +4,5 @@ public class Country
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    public List<City> Cities { get; set; }
+    public List<City> Cities { get; set; } = new List<City>();
 }
--- Code-along/L009_Logging_and_Tracking/Program.cs
+++ Code-along/L009_Logging_and_Tracking/Program.cs
@@ -1,15 +1,58 @@
 ﻿
 
 using L009_Logging_and_Tracking.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
 
 //AddCountry("Spain");
-UpdateDemo();
+//UpdateDemo();
+
+//RecreateDatabase();
+
+//AddCity("Stockholm", "Sweden");
+//AddCity("Gothenburg", "Sweden");
+//AddCity("Oslo", "Norway");
+//AddCity("Alingsås", "Sweden");
+//AddCity("Borås", "Sweden");
+
+//RemoveCity("Alingsås");
+
+Country myCountry = GetCountry("Sweden?!?");
+SaveCountry(myCountry);
+
+//myCountry.Name = "Sweden?!?????##";
+//myCountry.Cities.FirstOrDefault().Name = "NewCityName";
+
+//SaveCountry(new Country() { Name = "newCountry" });
+Console.WriteLine();
+
 
 static void RecreateDatabase()
 {
     using var db = new DemoContext();
     db.Database.EnsureDeleted();
     db.Database.EnsureCreated();
+}
+
+static Country? GetCountry(string countryName)
+{
+    using var db = new DemoContext();
+
+    return db.Countries.Include(c => c.Cities).FirstOrDefault(c => c.Name == countryName);
+}
+
+static void SaveCountry(Country country)
+{
+    if (country is null) return;
+
+    using var db = new DemoContext();
+
+    db.Countries.Update(country);
+
+    db.PrintChangeTrackerDebugInfo("Tracking info on new dbContext:");
+
+    //db.SaveChanges();
 }
 
 static void AddCountry(string name)
@@ -23,25 +66,62 @@ static void AddCountry(string name)
     db.SaveChanges();
 }
 
+static void AddCity(string cityName, string countryName)
+{
+    using var db = new DemoContext();
+
+    if (db.Cities.Any(c => c.Name == cityName))
+    {
+        return;
+    }
+
+    var city = new City() { Name = cityName };
+
+    var country = db.Countries.Include(c => c.Cities).FirstOrDefault(c => c.Name == countryName)
+        ?? new Country() { Name = countryName };
+
+    city.Country = country;
+
+    db.Cities.Add(city);
+
+    db.PrintChangeTrackerDebugInfo();
+
+    db.SaveChanges();
+}
+
+static void RemoveCity(string cityName)
+{
+    using var db = new DemoContext();
+
+    var city = db.Cities.FirstOrDefault(c => c.Name == cityName);
+
+    if (city is not null)
+    {
+        db.Cities.Remove(city);
+    }
+
+    db.PrintChangeTrackerDebugInfo();
+
+    db.SaveChanges();
+}
+
 static void UpdateDemo()
 {
     using var db = new DemoContext();
 
     var countries = db.Countries.ToList();
 
-    Console.WriteLine(db.ChangeTracker.DebugView.LongView);
+    //db.PrintChangeTrackerDebugInfo("Tracker status after loading data:");
 
-    countries[0].Name = "Sweden!";
-    countries[3].Name = "Spain!";
+    countries[0].Name = "Sweden?";
+    countries[2].Name = "Spain!sdf?";
 
-    db.ChangeTracker.DetectChanges();
+    db.Countries.Remove(countries[2]);
 
-    Console.WriteLine("\nTracker status after updates: ");
-    Console.WriteLine(db.ChangeTracker.DebugView.LongView);
+    db.PrintChangeTrackerDebugInfo("Tracker status after updates:");
 
-    db.SaveChanges();
+    //db.SaveChanges();
 
-    Console.WriteLine("\nTracker status after save: ");
-    Console.WriteLine(db.ChangeTracker.DebugView.LongView);
+    //db.PrintChangeTrackerDebugInfo("Tracker status after save:");
 
 }
