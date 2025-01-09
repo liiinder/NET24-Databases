266a8db759d621ba8d8e58b3957f1e07ed58cbef
Fredrik Johansson <fredrik@everyloop.com>
2025-01-09 10:58:24 +0100

    Add L013

---
+++ Code-along/L013_RepositoryPattern/Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.EntityFrameworkCore;
+
+namespace L011_MongoDB_EFcore.Models;
+
+[Collection("movies")]
+class Movie()
+{
+    //[BsonId]
+    public ObjectId Id { get; set; }
+
+    [BsonElement("title")]
+    public string Title { get; set; }
+
+    [BsonElement("year")]
+    public int Year { get; set; }
+
+    [BsonElement("imdb")]
+    public IMDB IMDB { get; set; }
+
+    [BsonIgnore]
+    public string UnmappedProperty { get; set; }
+}
---
+++ Code-along/L013_RepositoryPattern/Repository/IRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L013_RepositoryPattern.Repository
+{
+    internal interface IRepository<TEntity> where TEntity : class 
+    {
+        TEntity Get(int id);
+
+        TEntity Get(string id);
+
+        IEnumerable<TEntity> GetAll();
+
+        IEnumerable<TEntity> Find(Func<TEntity, bool> predicate);
+
+        void Add(TEntity entity);
+
+        void AddRange(IEnumerable<TEntity> entities);
+
+        void Remove(TEntity entity);
+
+        void RemoveRange(IEnumerable<TEntity> entities);
+    }
+}
