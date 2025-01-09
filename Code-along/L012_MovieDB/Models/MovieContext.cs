4588aa3a0c5917b794570984f863d6b4dc326087
Fredrik Johansson <fredrik@everyloop.com>
2025-01-09 10:12:31 +0100

    Add L012

---
+++ Code-along/L012_MovieDB/Models/MovieContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MongoDB.EntityFrameworkCore.Extensions;
+
+namespace MovieDB.Models;
+
+internal class MovieContext : DbContext
+{
+    public DbSet<Movie> Movies { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseMongoDB("mongodb+srv://fredrik:fredrik@cluster0.9zffbjc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", "sample_mflix");
+    }
+
+
+    //protected override void OnModelCreating(ModelBuilder modelBuilder)
+    //{
+    //    modelBuilder.Entity<Movie>().ToCollection("movies");
+    //    modelBuilder.Entity<Movie>().Property(m => m.Title).HasElementName("title");
+    //    modelBuilder.Entity<Movie>().Property(m => m.Year).HasElementName("year");
+    //    modelBuilder.Entity<Movie>().Property(m => m.Length).HasElementName("runtime");
+    //    modelBuilder.Entity<Movie>().Property(m => m.Plot).HasElementName("fullplot");
+    //    modelBuilder.Entity<Movie>().Property(m => m.Genres).HasElementName("genres");
+    //    //modelBuilder.Entity<Movie>().Property(m => m.IMDB).HasElementName("imdb");
+
+    //}
+}
