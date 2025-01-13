a5ce92cbb055c36e1eb47ede9bcb2c718bc412b7
Fredrik Johansson <fredrik@everyloop.com>
2025-01-13 10:04:38 +0100

    Add project L014_MongoDbDriver

---
+++ Code-along/L014_MongoDbDriver/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using MongoDB.Driver;
+
+var connectionString = "mongodb+srv://fredrik:fredrik@cluster0.9zffbjc.mongodb.net/";
+
+var client = new MongoClient(connectionString);
+
+client.ListDatabaseNames().ToList().ForEach(d => Console.WriteLine(d));
+
+var movieCollection = client.GetDatabase("sample_mflix").GetCollection<Movie>("movies");
+
+var filter = Builders<Movie>.Filter.Eq("title", "The Italian");
+
+var myMovie = movieCollection.Find(filter).FirstOrDefault();
+
+Console.WriteLine();
